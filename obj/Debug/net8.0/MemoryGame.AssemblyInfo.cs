ae983545a8a2e400b30e6d928122eccce8c30d3f
tomson218 <tomasz.pstrowski20@gmail.com>
2025-04-02 20:03:20 +0200

    delete uploaded images

--- obj/Debug/net8.0/MemoryGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MemoryGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c667eed209543fea4b9ffdc04cab040f72328ba9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21914dbf324108b5ecae48ef273209eece06adb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
