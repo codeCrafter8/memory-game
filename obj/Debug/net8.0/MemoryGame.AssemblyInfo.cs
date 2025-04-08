732bf75b6f0ca24c5630fd37ba05736ad4ff2661
fafusinho <krzysiek10911@gmail.com>
2025-04-08 10:54:43 +0200

    Card sets better behaviour

--- obj/Debug/net8.0/MemoryGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MemoryGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21914dbf324108b5ecae48ef273209eece06adb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae983545a8a2e400b30e6d928122eccce8c30d3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
